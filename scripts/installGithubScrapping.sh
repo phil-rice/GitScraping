04de1c260b569f35271a03610e94804ef020685b
phil <phil.rice@validoc.org>
2020-07-11 12:02:23 +0200

    trying to get working on azure devops

--- scripts/installGithubScrapping.sh
+++ scripts/installGithubScrapping.sh
@@ -1,6 +1,7 @@
 echo "making directory `pwd`/bin (making symbolic links to scripts and adding that directory to the path)"
 echo "WARNING: You must run this as source if you want it to change the path, other wise you will have to add bin to the path separately"
 
+pushd
 if [ ! -d "scripts" ]; then
     echo "Needs to have a scripts subdirectory"
     exit 2
@@ -13,3 +14,4 @@ find ../scripts -maxdepth 3 -type f  -name "*.sh" -exec ln -s {}  \;
 sudo chmod u+x ../scripts
 sudo chmod u+x .
 export PATH="$bindir:$PATH"
+popd
